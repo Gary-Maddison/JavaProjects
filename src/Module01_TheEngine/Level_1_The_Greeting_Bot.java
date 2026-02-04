aceb2612587a15dd0bad580a0e68bc8ef8bf8a3f
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-04 14:46:42 +0000

    Level 1 - The Greeting Bot

---
+++ src/Module01_TheEngine/Level_1_The_Greeting_Bot.java
@@ -0,0 +1,21 @@
+/*
+Your Task: Write a program that prints exactly the following three lines to the console. You must use at least
+one variable of type String to store the name "Amigo".
+
+Hello, my name is Amigo.
+I am learning Java 25.
+It's cool to be a programmer! 😎
+ */
+
+package Module01_TheEngine;
+
+public class Level_1_The_Greeting_Bot {
+
+    void main() {
+        String name = "Amigo";
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am learning Java 25.");
+        System.out.print("It's cool to be a programmer! \uD83D\uDE0E");
+    }
+}
+
