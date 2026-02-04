a1785e2b78345133d507ee87110be9dfcbfcf55a
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-04 11:11:01 +0000

    Success: Fixed SDK configuration and ran first project.

--- src/project01_basics/BasicSyntax.java
+++ src/project01_basics/BasicSyntax.java
@@ -3,7 +3,7 @@ package project01_basics;
 public class BasicSyntax {
     public static void main(String[] args) {
         String hello = "Hello "; // You named the variable 'hello'
-        System.out.println(hello + "World!"); // So you must use 'hello' here
+        System.out.println(hello + "World!!!"); // So you must use 'hello' here
     }
 }
 
