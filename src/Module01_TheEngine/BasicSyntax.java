3d1ac505f4acca3717610648416b860b94d39920
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-04 12:54:38 +0000

    Success: Fixed SDK configuration and ran first project.

---
+++ src/Module01_TheEngine/BasicSyntax.java
@@ -0,0 +1,10 @@
+package Module01_TheEngine;
+
+public class BasicSyntax {
+    void main() {
+
+        String hello = "Hello "; // You named the variable 'hello'
+        System.out.println(hello + "World!!!");  // So you must use 'hello' here
+    }
+}
+
--- src/project01_basics/BasicSyntax.java
+++
@@ -1,9 +0,0 @@
-package project01_basics;
-
-public class BasicSyntax {
-    public static void main(String[] args) {
-        String hello = "Hello "; // You named the variable 'hello'
-        System.out.println(hello + "World!!!"); // So you must use 'hello' here
-    }
-}
-
